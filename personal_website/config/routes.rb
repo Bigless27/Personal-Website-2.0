bc7262c5d7f02df44c2576bfb1541c2d72d0026b
Bigless27 <lessonryan@gmail.com>
2016-01-26 00:36:34 -0500

    Add user login and logout feature

--- personal_website/config/routes.rb
+++ personal_website/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get "welcome/index"
 
+  resources :users, only: [:new]
+  resources :sessions, only: [:new, :create]
+  delete '/logout', to: 'sessions#destroy'
+
   root :to => 'welcome#index'
 end
