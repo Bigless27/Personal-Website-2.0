77b82cfc97b847a6fdbfed255a00480ea5f3f0a4
Bigless27 <lessonryan@gmail.com>
2016-02-04 17:45:41 -0500

    Add seeds and coulmn for Blogs and Blog table

--- personal_website/db/schema.rb
+++ personal_website/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160126044735) do
+ActiveRecord::Schema.define(version: 20160204220429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160126044735) do
     t.string   "body",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "date"
   end
 
 end
