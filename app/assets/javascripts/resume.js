8db1cf7f68acda9b771a55c7702d85b9e07e0a27
Bigless27 <lessonryan@gmail.com>
2016-02-13 00:47:58 -0500

    Open resume link in new tab functionality

--- app/assets/javascripts/resume.js
+++ app/assets/javascripts/resume.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $('nav').children().eq(3).on('click', function(event){
     event.preventDefault;
     swal({
-      title: "<a style='color:#575c7a' href='https://www.dropbox.com/s/c8095pa2vma5z3v/Resume%202016%20PDF.pdf?dl=0'>Click Here To View Resume</a>",
+      title: "<a style='color:#575c7a' target='_blank' href='https://www.dropbox.com/s/c8095pa2vma5z3v/Resume%202016%20PDF.pdf?dl=0'>Click Here To View Resume</a>",
       html: true
     })
   })
