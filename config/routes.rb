94c060840173dac699fa458056bd06941d5c74db
Bigless27 <lessonryan@gmail.com>
2016-02-22 13:24:49 -0500

    Delete uneeded routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   get "welcome/index"
 
   resources :blogs, only: [:index, :show]
-  resources :users, only: [:new]
-  resources :sessions, only: [:new, :create]
-  delete '/logout', to: 'sessions#destroy'
 
   root :to => 'welcome#index'
 end
