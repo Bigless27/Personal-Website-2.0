6e45415efd1973aa4c8a38b48bb7c5ef925adedc
Bigless27 <lessonryan@gmail.com>
2016-03-06 21:47:47 -0600

    Seed Cheatsheet and url data

---
+++ db/seeds/url_seeds.rb
@@ -0,0 +1,3 @@
+Blog.find_by(title:"20 Hour Rule").urls.create(title: "20 Hour Rule", link: "https://www.youtube.com/watch?v=5MgBikgcWnY")
+
+Blog.find_by(title: "Heading to Chicago").urls.create(title: "Code Every Day (blog)", link: "http://ejohn.org/blog/write-code-every-day/")
