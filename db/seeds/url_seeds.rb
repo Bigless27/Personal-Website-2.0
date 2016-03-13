5d0a90c274766b63eb53e95be66a7f2badbf35a0
Bigless27 <lessonryan@gmail.com>
2016-03-12 23:52:06 -0600

    Seed blog 13

--- db/seeds/url_seeds.rb
+++ db/seeds/url_seeds.rb
@@ -1,3 +1,6 @@
 Blog.find_by(title:"20 Hour Rule").urls.create(title: "20 Hour Rule", link: "https://www.youtube.com/watch?v=5MgBikgcWnY")
 
 Blog.find_by(title: "Heading to Chicago").urls.create(title: "Code Every Day (blog)", link: "http://ejohn.org/blog/write-code-every-day/")
+
+
+Blog.find_by(title: "Interviews").urls.create(title: "POKE ROCK PAPER SCISSORS", link: "pokerockpaperscissors.herokuapp.com")
